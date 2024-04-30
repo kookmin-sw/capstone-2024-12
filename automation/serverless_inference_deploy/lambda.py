b174304c60ad1ede9e1b1a23cce63b0212607d9d
jhM00n <answlgns2056@kookmin.ac.kr>
2024-04-30 16:37:38 +0900

    terraform 생성 및 삭제에 따라 endpoint url을 db에 등록 및 삭제 하도록 업데이트

--- automation/serverless_inference_deploy/lambda.py
+++ automation/serverless_inference_deploy/lambda.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
-import boto3
 import json
-import uuid
+import requests
+
+# Terraform 바이너리의 전체 경로 설정
+terraform_binary = '/var/task/terraform'
 
 # terraform module 설정
 local_dir = '/tmp'
@@ -13,13 +15,13 @@ subprocess.run(["ln", "-s", "/var/task/terraform-provider-aws_v5.43.0_x5", ".ter
 subprocess.run(["ln", "-s", "/var/task/.terraform.lock.hcl"])
 subprocess.run(["ln", "-s", "/var/task/main.tf", "/tmp"])
 
-def create_backend(username, model, hash):
+def create_backend(user_uid, type, model_uid, endpoint_name):
 # Terraform backend 생성
     terraform_backend = f"""
     terraform {{
     backend "s3" {{
         bucket = "sskai-terraform-state"
-        key = "serverless_inference/{username}/{model}/{hash}/terraform.state"
+        key = "{user_uid}/{model_uid}/{type}/{endpoint_name}/terraform.state"
         region = "ap-northeast-2"
         encrypt = true
     }}
@@ -31,47 +33,70 @@ def create_backend(username, model, hash):
         f.write(terraform_backend)
 
 def handler(event, context):
-
-    endpoint_name = event["queryStringParameters"]["ENDPOINT_NAME"]
-    type = event["queryStringParameters"]["TYPE"]
-    username = event["queryStringParameters"]["USERNAME"]
-    action = event["queryStringParameters"]["ACTION"]
-    container_registry = event["queryStringParameters"]["CONTAINER_REGISTRY"]
-    model_name = event["queryStringParameters"]["MODEL_NAME"]
-    
-    # hash 생성
-    # hash = str(uuid.uuid4())
-    hash = 1234
-
-    # Terraform 바이너리의 전체 경로 설정
-    terraform_binary = '/var/task/terraform'
+    params = event.get("queryStringParameters", {})
+    user_uid = params.get("USER_UID")
+    endpoint_name = params.get("ENDPOINT_NAME")
+    model_uid = params.get("MODEL_UID")
+    type = params.get("TYPE")
+    action = params.get("ACTION")
 
     # backend 생성
-    create_backend(username, model_name, hash)
+    create_backend(user_uid, type, model_uid, endpoint_name)
 
-    # Terraform init
-    subprocess.run([terraform_binary, "init", "-reconfigure"])
+    inference_uid = params.get("INFERENCE_UID")
 
-    # Terraform apply
-    if action == 'create':
-        subprocess.run([terraform_binary, "apply", "-auto-approve"])
+    if inference_uid:        
+        # Terraform destroy
+        if action == 'destroy':
+            subprocess.run([terraform_binary, "destroy", "-auto-approve"])
+            
+            delete_inference_url = "https://wpcwvjlvkl.execute-api.ap-northeast-2.amazonaws.com/sskai-api-dev/inferences/"+inference_uid
+            response = requests.delete(delete_inference_url)
+
+            return {
+            'statusCode': 200,
+            'body': 'Terraform Destroyed Successfully'
+            }
         
-        # Terraform 출력값 가져오기
-        output = subprocess.check_output([terraform_binary, "output", "-json"])
-        outputs = json.loads(output.decode('utf-8'))
+        else:
+            return{
+                'statusCode': 201,
+                'body': 'Please check the action'
+            }
+        
+    else:        
+        # Terraform init
+        subprocess.run([terraform_binary, "init", "-reconfigure"])
 
-        # 'function_url' 출력값 추출
-        endpoint_url = outputs['function_url']['value']
+        # Terraform apply
+        if action == 'create':
+            subprocess.run([terraform_binary, "apply", "-auto-approve"])
+            
+            # Terraform 출력값 가져오기
+            output = subprocess.check_output([terraform_binary, "output", "-json"])
+            outputs = json.loads(output.decode('utf-8'))
 
-        return {
-        'statusCode': 200,
-        'body': f'Endpoint URL: {endpoint_url}'
-        }
-    
-    # Terraform destroy
-    if action == 'destroy':
-        subprocess.run([terraform_binary, "destroy", "-auto-approve"])
-        return {
-        'statusCode': 200,
-        'body': 'Terraform Destroyed Successfully'
-        }
+            # 'function_url' 출력값 추출
+            endpoint_url = outputs['function_url']['value']
+
+            add_inferences = {
+                "user": user_uid,
+                "name": endpoint_name,
+                "model": model_uid,
+                "type": type,
+                "endpoint": endpoint_url
+            }
+
+            add_inference_url="https://wpcwvjlvkl.execute-api.ap-northeast-2.amazonaws.com/sskai-api-dev/inferences"
+            response = requests.post(add_inference_url, json=add_inferences)
+
+            return {
+            'statusCode': 200,
+            'body': f'Endpoint URL: {endpoint_url}'
+            }
+        
+        else:
+            return {
+                'statusCode': 201,
+                'body': 'Please check the action'
+            }
