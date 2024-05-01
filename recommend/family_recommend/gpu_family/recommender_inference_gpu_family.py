1d0e42ce1ee73ffbd6dddff2dfeeb58436f52153
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2024-05-01 14:46:19 +0900

    modify gpu instance recommend algorithm for use gpu benchmark

--- recommend/family_recommend/gpu_family/recommender_inference_gpu_family.py
+++ recommend/family_recommend/gpu_family/recommender_inference_gpu_family.py
@@ -1,24 +1,42 @@
 from utils_inference_family import *
 
-def get_cheapest_instance_set_per_memory(df):
-    # df 에는 미리 전처리가 되어있어야 합니다.
-    # e.g.) SPS 2이상의 인스턴스들만 필터링 되어있고 price와 해당 인스턴스의 spec 등이 미리 병합되어 있어야 합니다.
-    
-    instance_set = set()
-    for memory in df['MemoryGiB'].unique():
-        df_great_equal = df[df['MemoryGiB'] >= memory]
-        for i, row in df_great_equal[df_great_equal['SpotPrice'] <= df_great_equal['SpotPrice'].min()].iterrows():
-            instance_set.add(row['InstanceType'])
-    
-    return instance_set
+def get_benchmark(row):
+    gpu_model_name = row['GPUModel']
+    gpu_count = row['GPUCount']
+    benchmark = get_gpu_benchmark(gpu_model_name, gpu_count)
+    return benchmark
+
+def get_total_instance_score(row, min_price, max_price, min_benchmark, max_benchmark):
+    price = row['SpotPrice']
+    benchmark = row['Benchmark']
+    sps = row['SPS']
+
+    # 선형 보간을 위한 alpha 값
+    # 스팟 가격과 벤치마크 점수를 어떤 비율로 반영할지 결정합니다.
+    alpha = 0.7
+    price_score = 1 - ((price - min_price) / (max_price - min_price))
+    benchmark_score = (benchmark - min_benchmark) / (max_benchmark - min_benchmark)
+    # 선형 보간 적용 및 sps 에 높은 비중을 주기 위해서 sps 값을 곱해줍니다.
+    total_score = sps * ((1 - alpha) * price_score + alpha * benchmark_score)
+
+    return total_score
 
 # 해당 리전에 대해 Karpenter 가 스케줄링 할 Instance 목록을 제공합니다.
 def get_gpu_instance_family_for_inference(region):
     price_df = get_price_df(region)
-    price_df = price_df[price_df['SPS'] >= 2]
+    #price_df = price_df[price_df['SPS'] >= 2]
     instance_df = get_instance_df(region)
 
     df = pd.merge(price_df, instance_df, on=['Region', 'InstanceType'], how='inner')
+
+    # benchmark 및 total score 계산
+    df['Benchmark'] = df.apply(get_benchmark, axis=1)
+    min_price = df['SpotPrice'].min()
+    max_price = df['SpotPrice'].max()
+    min_benchmark = df['Benchmark'].min()
+    max_benchmark = df['Benchmark'].max()
+    df['TotalScore'] = df.apply(get_total_instance_score, args=(min_price, max_price, min_benchmark, max_benchmark), axis=1)
+
     # architecture 가 x86 인 것만 제공
     df_exploded = df.explode('SupportedArchitectures')
     df = df.loc[df_exploded[df_exploded['SupportedArchitectures'].isin(['x86_64'])].index.unique()]
@@ -26,12 +44,19 @@ def get_gpu_instance_family_for_inference(region):
     df = df[df['GPUManufacturer'] == 'NVIDIA'].reset_index(drop=True)
     df = df[df['GPUCount'] == 1].reset_index(drop=True)
     
-    family = set()
+    df['MaxTotalScore'] = df.groupby(['InstanceType', 'Region'])['TotalScore'].transform('max')
+    # 최종 점수 기준 내림차순 정렬
+    sorted_df_by_total_score = df[df['TotalScore'] == df['MaxTotalScore']].sort_values(by='TotalScore', ascending=False)
 
-    for gpu_memory in df['TotalGPUMemoryGiB'].unique():
-        df_great_equal_gpu_memory = df[df['TotalGPUMemoryGiB'] >= gpu_memory]
-        family_per_gpu_memory = get_cheapest_instance_set_per_memory(df_great_equal_gpu_memory)
-        
-        family |= family_per_gpu_memory
-    
-    return list(family)
+    family_list = []
+    num_of_family = 5
+    for index, row in sorted_df_by_total_score.iterrows():
+        family_list.append(row['InstanceType'])
+        num_of_family -= 1
+        if num_of_family <= 0:
+            break
+
+    return family_list
+
+if __name__ == "__main__":
+    family_list = get_gpu_instance_family_for_inference("ap-northeast-2")
