c21810508c87e069e3c9515373edf3db8c86c4cb
mh3ong <mhsong@kookmin.ac.kr>
2024-05-08 22:30:37 +0900

    move regression request.py

---
+++ inference/template_code/regression/request.py
@@ -0,0 +1,26 @@
+import requests
+import torch
+import pickle
+import base64
+import time
+
+x = torch.randn(1,1)
+print(f"X Value is : {x}")
+serialized_tensor = pickle.dumps(x)
+
+url = 'http://localhost:8080/'
+
+request_body = {
+    "body": base64.b64encode(serialized_tensor).decode('utf-8')
+}
+# print(request_body)
+
+end_to_end_latency_time_start = time.time()
+response = requests.post(url, json=request_body).json()
+end_to_end_latency_time = time.time() - end_to_end_latency_time_start
+print(response)
+pickle_y = base64.b64decode(response['body'].encode('utf-8'))
+y = pickle.loads(pickle_y)
+
+print(f"Y Value is : {y}")
+print(f"End-to-end Latency time is : {end_to_end_latency_time}")
