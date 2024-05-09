89230b8ee5f38368db0e881146601c31f8670ab4
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2024-05-09 20:38:10 +0900

    remove recommend_function 2~5 and IaC

--- recommend/family_recommend/IaC/output.tf
+++ recommend/family_recommend/IaC/output.tf
@@ -5,19 +5,3 @@
 output "recommend_family_1_function_url" {
   value = module.recommend_family_1.function_url
 }
-
-output "recommend_family_2_function_url" {
-  value = module.recommend_family_2.function_url
-}
-
-output "recommend_family_3_function_url" {
-  value = module.recommend_family_3.function_url
-}
-
-output "recommend_family_4_function_url" {
-  value = module.recommend_family_4.function_url
-}
-
-output "recommend_family_5_function_url" {
-  value = module.recommend_family_5.function_url
-}
