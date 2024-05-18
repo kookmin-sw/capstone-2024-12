589d2c8617d4182833eabfe91089bf81695b7e4b
mhsong <mhsong@kookmin.ac.kr>
2024-05-19 01:14:34 +0900

    Add tfstate in deploy_db_api

--- automation/deploy_db_api/provider.tf
+++ automation/deploy_db_api/provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = var.region
   profile = var.awscli_profile
 }
+
+terraform {
+  backend "s3" {
+    bucket  = "sskai-terraform-state"
+    key     = "deploy_db_api/tf.state"
+    region  = "ap-northeast-2"
+    encrypt = true
+  }
+}
