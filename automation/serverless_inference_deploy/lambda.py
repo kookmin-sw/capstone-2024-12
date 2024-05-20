a582878452aa6f768a4da0638cdad82e8b1101d9
mhsong <mhsong@kookmin.ac.kr>
2024-05-21 00:28:23 +0900

    Fix error

--- automation/serverless_inference_deploy/lambda.py
+++ automation/serverless_inference_deploy/lambda.py
@@ -39,22 +39,6 @@ def create_backend(user_uid, endpoint_uid):
     with open("backend.tf", "w") as f:
         f.write(terraform_backend)
 
-def create_var_json(prefix, container_registry, ram_size, model_s3_url):
-    terraform_vars = {
-        "prefix": prefix,
-        "container_registry": container_registry,
-        "lambda_ram_size": ram_size,
-        "model_s3_url": model_s3_url,
-        "region": REGION
-    }
-    
-    var_file_path = f"/tmp/{prefix}.tfvars.json"
-    
-    with open(var_file_path, 'w') as json_file:
-        json.dump(terraform_vars, json_file, indent=4)
-    
-    return var_file_path
-
 def handler(event, context):
     body = json.loads(event.get("body", "{}"))
     user_uid = body.get("user")
@@ -66,14 +50,12 @@ def handler(event, context):
     # backend 생성
     create_backend(user_uid, endpoint_uid)
 
-    var_file_path = create_var_json(endpoint_uid, container_registry, ram_size, model_s3_url)
-
     # Terraform init
     subprocess.run([terraform_binary, "init", "-reconfigure"])    
     
     # Terraform apply
     if action == 'create':
-        subprocess.run([terraform_binary, "apply", "--var", f"prefix={endpoint_uid}", "--var", f"container_registry={container_registry}", "--var", f"lambda_ram_size={ram_size}", "--var", f"model_s3_url={model_s3_url}", "-auto-approve"])
+        subprocess.run([terraform_binary, "apply", "--var", f"prefix={endpoint_uid}", "--var", f"container_registry={container_registry}", "--var", f"lambda_ram_size={ram_size}", "--var", f"model_s3_url={model_s3_url}", "--var", f"region={REGION}", "-auto-approve"])
 
         # Terraform 출력값 가져오기
         output = subprocess.check_output([terraform_binary, "output", "-json"])
