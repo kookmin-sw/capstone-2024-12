4b15f77e8b7e08453a5ef4e5e8d025bebfe79417
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2024-05-21 14:26:09 +0900

    add parameter store for cost monitoring

--- recommend/family_recommend/IaC/main.tf
+++ recommend/family_recommend/IaC/main.tf
@@ -21,7 +21,7 @@
 module "recommend_family" {
   source = "github.com/kookmin-sw/capstone-2024-12//IaC/serverless_api_template"
   prefix = "recommend_family"
-  container_registry = "694448341573.dkr.ecr.ap-northeast-2.amazonaws.com"
+  container_registry = var.container_registry
   container_repository = "recommend-family"
   container_image_tag = "latest"
   lambda_ram_size = 256
@@ -30,17 +30,38 @@ module "recommend_family" {
 }
 
 resource "aws_ssm_parameter" "param_recommend_family_lambda_function_url" {
-  name = "recocommend_family_lambda_function_url"
+  name = "recommend_family_lambda_function_url"
   type = "String"
   value = module.recommend_family.function_url
 
   depends_on = [ module.recommend_family ]
 }
 
-provider "aws" {
-    region = var.region
-    profile = var.awscli_profile
+variable "nodepool_numbers" {
+  type    = list(string)
+  default = ["1", "2", "3", "4", "5"]
 }
+
+resource "aws_ssm_parameter" "nodepool_ondemand_price" {
+  for_each = toset(var.nodepool_numbers)
+  
+  name  = "nodepool_${each.value}_ondemand_price"
+  type  = "String"
+  value = "9999"
+}
+
+resource "aws_ssm_parameter" "nodepool_spot_price" {
+  for_each = toset(var.nodepool_numbers)
+  
+  name  = "nodepool_${each.value}_spot_price"
+  type  = "String"
+  value = "9999"
+}
+
+# provider "aws" {
+#     region = var.region
+#     profile = var.awscli_profile
+# }
 
 terraform {
   backend "s3" {
